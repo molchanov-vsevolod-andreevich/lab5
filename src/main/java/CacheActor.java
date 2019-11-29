7ad93d9dfdfa275c06ea8255927795d93966d14c
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-29 13:05:09 +0300

    Scripted auto-commit on change (2019-11-29 13:05:09) by gitwatch.sh

--- src/main/java/CacheActor.java
+++ src/main/java/CacheActor.java
@@ -2,12 +2,11 @@ import akka.actor.AbstractActor;
 import akka.actor.Props;
 import akka.japi.pf.ReceiveBuilder;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CacheActor extends AbstractActor {
-    private Map<String, ArrayList<TestPackageResponse.TestResult>> store = new HashMap<>();
+    private Map<String, Integer> store = new HashMap<>();
 
     static Props props() {
 
