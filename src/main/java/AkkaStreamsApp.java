c9000623b5adc19e5f6da1ce6783aa8e890a344a
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-30 00:01:02 +0300

    Scripted auto-commit on change (2019-11-30 00:01:02) by gitwatch.sh

--- src/main/java/AkkaStreamsApp.java
+++ src/main/java/AkkaStreamsApp.java
@@ -40,8 +40,8 @@ public class AkkaStreamsApp {
                 .mapAsync()
                 .map(r -> {
                     cacheActor.tell(r, ActorRef.noSender());
-                        return HttpResponse.create()
-                                .withEntity(r.getUrl + " " + r.getPing)
+                    return HttpResponse.create()
+                            .withEntity(r.getUrl + " " + r.getPing);
                 })
     }
 
