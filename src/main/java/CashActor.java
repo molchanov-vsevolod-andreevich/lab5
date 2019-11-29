be696e3e4e1cffe8cea1872510dd3f99f6b13e61
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-29 12:56:10 +0300

    Scripted auto-commit on change (2019-11-29 12:56:10) by gitwatch.sh

--- src/main/java/CashActor.java
+++ src/main/java/CashActor.java
@@ -1,8 +1,30 @@
 import akka.actor.AbstractActor;
+import akka.actor.Props;
+import akka.japi.pf.ReceiveBuilder;
 
 public class CashActor extends AbstractActor {
+
+    static Props props() {
+
+        return Props.create(CashActor.class);
+    }
+
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
+                .match(StoreActor.GetMessage.class, req -> {
+                    storeActor.tell(req, sender());
+                })
+                .match(TestPackageRequest.class, msg -> {
+                    for (TestPackageRequest.Test test : msg.getTests()) {
+                        runTestActor.tell(new TestToEval(msg.getPackageId(),
+                                        msg.getJsScript(),
+                                        msg.getFunctionName(),
+                                        test),
+                                self());
+                    }
+                })
+                .build();
+    }
     }
 }
