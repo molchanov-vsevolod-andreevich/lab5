90bfabae93a14761f73e1682d26aa4a7fd3c1cd7
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-29 13:26:36 +0300

    Scripted auto-commit on change (2019-11-29 13:26:36) by gitwatch.sh

--- src/main/java/TestPing.java
+++ src/main/java/TestPing.java
@@ -1,2 +1,17 @@
 public class TestPing {
+    private String url;
+    private Integer ping;
+
+    public ResultPing(String url, Integer ping) {
+        this.url = url;
+        this.ping = ping;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Integer getPing() {
+        return ping;
+    }
 }
