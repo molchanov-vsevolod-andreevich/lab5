b10897140c3c684deae6afe1129557bbf16ddb52
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-29 13:08:15 +0300

    Scripted auto-commit on change (2019-11-29 13:08:15) by gitwatch.sh

--- src/main/java/CacheActor.java
+++ src/main/java/CacheActor.java
@@ -17,16 +17,10 @@ public class CacheActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(HttpResponse.class, req -> {
-                    storeActor.tell(req, sender());
+                    
                 })
                 .match(HttpRequest.class, msg -> {
-                    for (TestPackageRequest.Test test : msg.getTests()) {
-                        runTestActor.tell(new TestToEval(msg.getPackageId(),
-                                        msg.getJsScript(),
-                                        msg.getFunctionName(),
-                                        test),
-                                self());
-                    }
+
                 })
                 .build();
     }
