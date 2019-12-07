247ee81514a7907f6a4914a1b90369b84f4b791a
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-12-07 12:14:36 +0400

    Scripted auto-commit on change (2019-12-07 12:14:36) by gitwatch.sh

--- src/main/java/AkkaStreamsApp.java
+++ src/main/java/AkkaStreamsApp.java
@@ -7,7 +7,7 @@ import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.model.Query;
-import akka.pattern.Patterns$;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
