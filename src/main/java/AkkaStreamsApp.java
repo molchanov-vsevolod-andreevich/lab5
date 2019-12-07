84acc9d7fdc5ae4bd40db8f01ee7ea4cd2786563
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-12-07 12:35:16 +0400

    Scripted auto-commit on change (2019-12-07 12:35:16) by gitwatch.sh

--- src/main/java/AkkaStreamsApp.java
+++ src/main/java/AkkaStreamsApp.java
@@ -58,6 +58,7 @@ public class AkkaStreamsApp {
                                 return CompletableFuture.completedFuture(res);
                             } else {
                                 return Source.from(Collections.singletonList(res))
+                                        .toMat()
                             }
                         }))
                 .map(res -> {
